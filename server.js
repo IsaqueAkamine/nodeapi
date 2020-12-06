e6c1aec5a2a2bdb8973a3f055b82111ce4ba92f7
Isaque Akamine <isaqueakamine@MacBook-Pro.local>
2020-12-06 03:09:20 -0300

    routes for products

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const { json } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
-// app.use(json);
+app.use(express.json());
 
 // Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
